4f8d93894b68b0bf224deed1b486f1af4846e871
raghavm07 <89573705+raghavm07@users.noreply.github.com>
2024-10-30 00:05:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ const App = () => {
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
             <Hero />
-          <Content />
           </div>
+          <Content />
           <Values />
           <Project />
           <Published />
